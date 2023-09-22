91bda2d677ba6ef012298c5b667f4d2a613cdd8c
charleslukes <charleslukes28@gmail.com>
2023-09-22 15:36:39 +0100

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: "src/main.ts",
+  output: {
+    file: "public/bundle.js",
+    format: "iife", // immediately-invoked function expression — suitable for <script> tags
+    sourcemap: true,
+    name: "GibEscrow"
+  },
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+      preferBuiltins: false,
+    }), 
+    commonjs(),
+    json(),
+    typescript(),
+    nodePolyfills({
+      exclude: '/node_modules'
+    }),
+    production && terser(), // minify, but only in production
+  ],
+};
