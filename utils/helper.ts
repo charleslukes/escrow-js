65954805c0238be761fd544ab55ac1ff37b135f0
charleslukes <charleslukes28@gmail.com>
2023-10-03 17:36:59 +0100

    add u64 number creator

--- utils/helper.ts
+++ utils/helper.ts
@@ -6,7 +6,17 @@ function randomString(length: number, chars: string) {
   
   
   export const randomBytes = (length: number) => {
-    var rString = randomString(length, '0123456789abcdefghijklmnopqrstuvwxyz');
+    let rString = randomString(length, '0123456789abcdefghijklmnopqrstuvwxyz');
     const buf1 = Buffer.from(rString, 'hex');
     return buf1
   }
+
+  export function randomU64(): number {
+    // JavaScript's Number can represent integers accurately up to 2^53 - 1.
+    // So, we'll generate two 32-bit numbers and combine them to simulate a 64-bit number.
+    const upper = Math.floor(Math.random() * 0x100000000); // Random 32-bit number
+    const lower = Math.floor(Math.random() * 0x100000000); // Random 32-bit number
+
+    // Combine the two 32-bit numbers
+    return upper * 0x100000000 + lower;
+}
